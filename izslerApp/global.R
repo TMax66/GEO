6a65f1d977c1e3ffc64c6bfa08d295974c70ff0d
TMax66 <massimo.tranquillo@gmail.com>
2019-07-18 15:40:39 +0200

    update

--- izslerApp/global.R
+++ izslerApp/global.R
@@ -11,20 +11,36 @@ library(DT)
 #library(janitor)
 library(RColorBrewer)
 
+coord <- read_excel("coord.xlsx")
 
-lomb<-readOGR(dsn="shp", layer="Province_2012_polygon")
+lomb<-readOGR(dsn="shp", layer="Province_2018")
 lomb<-spTransform(lomb, CRS("+proj=longlat +datum=WGS84"))
-emilia<-readOGR(dsn="shp", layer="V_LAM_GLI_Ed_2018_ES32")
+emilia<-readOGR(dsn="shp", layer="V_PRV_GPG")
 emilia<-spTransform(emilia, CRS("+proj=longlat +datum=WGS84"))
 
 
 leaflet(emilia) %>% addTiles() %>% 
   addPolygons(data=emilia) %>% 
+  addPolygons(data=lomb) %>% 
+  addMarkers( data=coord, ~long, ~lat, popup = ~as.character(sez), label = ~as.character(sez))
+
+leaflet(coord) %>% addTiles() %>% 
+  addMarkers(  ~long, ~lat, popup = ~as.character(sez), label = ~as.character(sez))
+ 
+
+
+# addPolygons(data=emilia) %>% 
   addPolygons(data=lomb)
 
 
 
-%>% 
+#%>% 
+  addMarkers(  ~long, ~lat, popup = ~as.character(sez), label = ~as.character(sez))
+
+
+
+
+#%>% 
   addPolygons(data=emilia, fillColor="",color="", fillOpacity = 0.9)
 
 
