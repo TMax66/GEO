3e9f75a02ec76a86eb2c6a644718831b57735bcc
TMax66 <massimo.tranquillo@gmail.com>
2020-04-21 10:27:19 +0200

    update

--- izslerApp/server.R
+++ izslerApp/server.R
@@ -3,16 +3,16 @@ server <- function(input, output, session) {
 
   
   # dx<-reactive({
-  #   avv %>% 
-  #     filter(anno==input$anno, specie==input$specie) %>% 
-  #     group_by(comune) %>% 
+  #   avv %>%
+  #     filter(anno==input$anno, specie==input$specie) %>%
+  #     group_by(comune) %>%
   #     summarise("casi"=n())
   # })
   # 
   # lab<-reactive({
-  #   avv %>% 
+  #   avv %>%
   #     filter(anno==input$anno, specie==input$specie) %>%
-  #     group_by(comune, sostanza) %>% 
+  #     group_by(comune, sostanza) %>%
   #     summarise("casi"=n())
   # })
   # 
@@ -21,18 +21,18 @@ server <- function(input, output, session) {
   # })
   # 
   # output$map <- renderLeaflet({
-  #   
+  # 
   #   polycom<-subset(BG, BG@data$NOME_COM %in% com())
-  #   
+  # 
   #   polycom@data %>% inner_join(lab(), by=c("NOME_COM"="comune"))
-  #   
+  # 
   #   pop<-polycom@data$NOME_COM
-  #   
-  #   leaflet(data=polycom) %>% addTiles() %>% 
-  #     addPolygons(data=polycom, fillColor="navy",color="", fillOpacity = 0.9, label=pop) %>% 
-  #     
-  #     addPolygons(data=BG,fill=F, color="gray", weight=1, opacity=1.0) %>% 
+  # 
+  #   leaflet(data=polycom) %>% addTiles() %>%
+  #     addPolygons(data=polycom, fillColor="navy",color="", fillOpacity = 0.9, label=pop) %>%
+  # 
+  #     addPolygons(data=BG,fill=F, color="gray", weight=1, opacity=1.0) %>%
   #     addPolygons(data=provincie, fill=F, color="blue", weight = 2)
-  #   
-  })
-})
+  # 
+  # })
+}
